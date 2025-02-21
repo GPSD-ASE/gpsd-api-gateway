681ebe3ed8d1d35acb0d56d700b4adb5a400c2cf
prkaaviya <prkaaviya17@gmail.com>
2025-02-21 02:22:27 +0100

    fix: change routes, add some hardcoded value for testing

--- internal/gateway/pkg/routes/routes.go
+++ internal/gateway/pkg/routes/routes.go
@@ -7,7 +7,8 @@ import (
 )
 
 func RegisterRoutes(r *mux.Router) {
-	r.HandleFunc("/register", handlers.RegisterHandler).Methods("POST") // non admin users (mobile) -> user service
-	r.HandleFunc("/login", handlers.LoginHandler).Methods("POST") // -> user service
-	r.HandleFunc("/logout", handlers.LogoutHandler).Methods("POST") // -> user service
+	r.HandleFunc("/register", handlers.RegisterHandler).Methods("POST") // forward to user service
+	r.HandleFunc("/register-admin", handlers.RegisterAdminHandler).Methods("POST") // forward to user service, add roleType in body
+	r.HandleFunc("/login", handlers.LoginHandler).Methods("POST") // -> forward to user service, return the token from user service
+	r.HandleFunc("/verify", handlers.VerifyHandler).Methods("POST") // -> for all users, logic in api-gateway
 }
