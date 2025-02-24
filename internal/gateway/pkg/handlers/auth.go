049e04042c6833ac4983c4fe2688be9eed9f8605
prkaaviya <prkaaviya17@gmail.com>
2025-02-24 13:43:37 +0000

    fix: placeholder code for frontend

--- internal/gateway/pkg/handlers/auth.go
+++ internal/gateway/pkg/handlers/auth.go
@@ -1,6 +1,69 @@
 package handlers
 
 import (
+	"encoding/json"
+	"net/http"
+)
+
+type Response struct {
+	Message     string `json:"message"`
+	AccessToken string `json:"accessToken,omitempty"`
+}
+
+type LoginRequest struct {
+	Username string `json:"username"`
+}
+
+func RegisterHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	response := Response{Message: "Register now as User! Work in progress."}
+	json.NewEncoder(w).Encode(response)
+}
+
+func RegisterAdminHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	response := Response{Message: "Register Now as Admin! Work in progress."}
+	json.NewEncoder(w).Encode(response)
+}
+
+func LoginHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+
+	var loginRequest LoginRequest
+	var response Response
+	err := json.NewDecoder(r.Body).Decode(&loginRequest)
+	if err != nil {
+		response = Response{Message: "Invalid JSON payload from user."}
+		w.WriteHeader(http.StatusBadRequest)
+		goto out
+	}
+
+	if loginRequest.Username == "user1" {
+		response = Response{Message: "Login forbidden!"}
+		w.WriteHeader(http.StatusForbidden)
+		goto out
+	}
+
+	response = Response{Message: "Login API is successful.", AccessToken: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjEwIiwidXNlcm5hbWUiOiJEdW1teSIsInJvbGUiOiIxIn0.isgyco7Uq5J4N-QIFW3RJ_JM7eD4WMAJoqDh0bxVrYo "}
+	w.WriteHeader(http.StatusOK)
+
+out:
+	json.NewEncoder(w).Encode(response)
+}
+
+func VerifyHandler(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
+	response := Response{Message: "Token verification is successful."}
+	json.NewEncoder(w).Encode(response)
+}
+
+/*
+package handlers
+
+import (
 	"bytes"
 	"encoding/json"
 	"fmt"
@@ -125,3 +188,4 @@ func VerifyHandler(w http.ResponseWriter, r *http.Request) {
 
 	forwardRequest(w, r, "/verify", nil)
 }
+*/
