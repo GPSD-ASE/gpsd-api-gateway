9061dd88e573896d5796f361cfea8551ea5a3644
prkaaviya <prkaaviya17@gmail.com>
2025-03-20 21:34:14 +0000

    fix: remove tls commented part

--- internal/main.go
+++ internal/main.go
@@ -1,43 +1,15 @@
 package main
 
 import (
-	"crypto/tls"
 	"log"
 	"net/http"
 	"os"
 
 	"gpsd-api-gateway/internal/gateway/pkg/config"
-	"gpsd-api-gateway/internal/gateway/pkg/handlers"
 	"gpsd-api-gateway/internal/gateway/pkg/routes"
 
 	"github.com/gorilla/mux"
 )
-
-// Start HTTPS Server for handling external requests.
-func startHTTPSServer() {
-	cert, err := handlers.RetrieveCertFromVault()
-	if err != nil {
-		log.Fatalf("Error retrieving certificates from Vault: %v", err)
-	}
-
-	r := mux.NewRouter()
-	routes.RegisterRoutes(r)
-
-	server := &http.Server{
-		Addr:    ":3000",
-		Handler: r,
-		TLSConfig: &tls.Config{
-			MinVersion:   tls.VersionTLS13,
-			Certificates: []tls.Certificate{*cert},
-		},
-	}
-
-	log.Println("HTTPS Server for API Gateway running on https://0.0.0.0:3000.")
-	err = server.ListenAndServeTLS("", "")
-	if err != nil {
-		log.Fatalf("HTTPS server error: %v", err)
-	}
-}
 
 // Start HTTP Server
 func startHTTPServer() {
@@ -65,5 +37,4 @@ func main() {
 	log.Printf("Vault is running at %s.", config.ApiGatewayConfig.VaultAddr)
 
 	startHTTPServer()
-	// startHTTPSServer()
 }
