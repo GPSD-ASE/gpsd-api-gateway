5c0e9cfcc717009f1cebd7dbd85c88ba81ff860d
Prakash Narasimhan <45827034+Prakash3008@users.noreply.github.com>
2025-04-11 14:18:44 +0100

    Update auth.go

--- internal/gateway/pkg/handlers/auth.go
+++ internal/gateway/pkg/handlers/auth.go
@@ -15,6 +15,8 @@ import (
 
 type UserData struct {
 	Username string `json:"username"`
+	Password string `json:"password"`
+	Email string `json:"email"`
 	Role     string `json:"role,omitempty"`
 }
 
